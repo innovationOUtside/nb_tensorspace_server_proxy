dd21f399172037e77bcdf28afcceb181639976c2
Tony Hirst <tony.hirst@gmail.com>
2023-02-27 11:49:54 +0000

    Remove notebook dependency

--- setup.py
+++ setup.py
@@ -4,11 +4,10 @@ from setuptools import setup
 setup(
     name="nb-tensorspace-playground",
     packages= ['nb_tensorspace_playground'],
-    version='0.0.1',
+    version='0.0.2',
     include_package_data=True,
     install_requires=[
-        'jupyter-server-proxy',
-        'notebook'
+        'jupyter-server-proxy'
     ],
     url="https://github.com/innovationOUtside/nb_tensorspace_server_proxy",
     author="",
